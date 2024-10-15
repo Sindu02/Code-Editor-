44961cc0066e59d3c3c1fa00568fca83fa5d8d68
Sindu02 <113755359+Sindu02@users.noreply.github.com>
2024-10-15 08:36:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+//components
+import Home from './components/home';
+import DataProvider from './context/dataProvider';
+
+function App() {
+  return (
+    <DataProvider>    
+      <Home/>
+    </DataProvider>
+  );
+}
+
+export default App;
